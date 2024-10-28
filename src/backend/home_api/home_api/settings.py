fe285c9ce5a3d6a044784ce560c9f4801d213f1e
toukoum <raphaelgiraud12@gmail.com>
2024-10-28 14:49:44 +0100

    docker compose

--- src/backend/home_api/home_api/settings.py
+++ src/backend/home_api/home_api/settings.py
@@ -102,6 +102,7 @@ TEMPLATES = [
     },
 ]
 
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -217,6 +218,10 @@ PASSWORD_RESET_CONFIRM_REDIRECT_BASE_URL = "http://localhost:5500/password-reset
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500"
+]
 
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
