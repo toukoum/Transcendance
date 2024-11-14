e254a5a0f1927c9d8881408e66015dbe353a4bc7
toukoum <raphaelgiraud12@gmail.com>
2024-11-14 12:23:51 +0100

    adding just base config

--- src/backend/home_api/games/views.py
+++ src/backend/home_api/games/views.py
@@ -10,29 +10,48 @@ from games.serializers import MatchSerializer
 from django.contrib.auth.models import User
 
 class MatchViewSet(viewsets.ModelViewSet):
-		queryset = Match.objects.all()
-		serializer_class = MatchSerializer
-		permission_classes = [IsAuthenticated]
+        queryset = Match.objects.all()
+        serializer_class = MatchSerializer
+        permission_classes = [IsAuthenticated]
 
-		def create(self, request, *args, **kwargs):
-				
-				player1 = request.user
+        def create(self, request, *args, **kwargs):
+                
+                player1 = request.user
 
-				opponent_username = request.data.get('opponent')
+                opponent_username = request.data.get('opponent')
 
-				try:
-						player2 = User.objects.get(username=opponent_username)
-				except User.DoesNotExist:
-						return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-				
-				if (player1 == player2):
-						return Response({'error': 'You cannot play against yourself'}, status=status.HTTP_400_BAD_REQUEST)	
+                try:
+                        player2 = User.objects.get(username=opponent_username)
+                except User.DoesNotExist:
+                        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+                
+                if (player1 == player2):
+                        return Response({'error': 'You cannot play against yourself'}, status=status.HTTP_400_BAD_REQUEST)	
+                
+                
+                # ongoing_matches_player1 = Match.objects.filter(
+                #    matchplayer__player_id=player1.id,
+                #    end_time__isnull=True
+                # )
 
-				match = Match.objects.create()
+                # if ongoing_matches_player1.exists():
+                #    return Response({'error': 'You already have a Game on going'})
+                
+                # ongoing_matches_player2 = Match.objects.filter(
+                #    matchplayer__player_id=player1.id,
+                #    end_time__isnull=True
+                # )
 
-				match_player1 = MatchPlayer.objects.create(match_id=match, player_id=player1)
-				match_player2 = MatchPlayer.objects.create(match_id=match, player_id=player2)
+                # if ongoing_matches_player2.exists():
+                #    return Response({'error': f'{opponent_username} is already in game'})
+                
+                
 
-				serializer = MatchSerializer(match)
+                match = Match.objects.create()
 
-				return Response(serializer.data, status=status.HTTP_201_CREATED)
+                match_player1 = MatchPlayer.objects.create(match_id=match, player_id=player1)
+                match_player2 = MatchPlayer.objects.create(match_id=match, player_id=player2)
+
+                serializer = MatchSerializer(match)
+
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
