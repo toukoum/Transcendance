cda11300fd87ad0ec5d8b698383e4bee03a9f760
Juiiceee <lbehr@student.42.fr>
2024-11-14 21:44:29 +0100

    a voir

--- src/blockchain/scripts.js
+++ src/blockchain/scripts.js
@@ -1,80 +1,93 @@
 let provider;
+let signer;
+let contract;
+const ABI = `[
+	{
+		"inputs": [],
+		"name": "decre",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "incre",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_number",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "number",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]`;
 
-// const connectWallet = async () => {
-// 	try {
-// 		if (typeof window.ethereum === 'undefined') {
-// 			alert("MetaMask n'est pas installé !");
-// 			return;
-// 		}
+const contractAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
 
-// 		provider = new ethers.providers.JsonRpcProvider();
-// 		await provider.send("eth_requestAccounts", []);
-// 		const signer = provider.getSigner();
-// 		console.log("Signer:", signer);
-// 		console.log("Adresse du compte:", await signer.getAddress());
-// 	} catch (error) {
-// 		console.error("Erreur lors de la connexion au portefeuille:", error);
-// 	}
-// };
-
-const jsonAbi = [
-		{
-			"inputs": [],
-			"name": "decre",
-			"outputs": [],
-			"stateMutability": "nonpayable",
-			"type": "function"
-		},
-		{
-			"inputs": [],
-			"name": "incre",
-			"outputs": [],
-			"stateMutability": "nonpayable",
-			"type": "function"
-		},
-		{
-			"inputs": [
-				{
-					"internalType": "uint256",
-					"name": "_number",
-					"type": "uint256"
-				}
-			],
-			"stateMutability": "nonpayable",
-			"type": "constructor"
-		},
-		{
-			"inputs": [],
-			"name": "number",
-			"outputs": [
-				{
-					"internalType": "uint256",
-					"name": "",
-					"type": "uint256"
-				}
-			],
-			"stateMutability": "view",
-			"type": "function"
+const connectWallet = async () => {
+	try {
+		if (typeof window.ethereum === 'undefined') {
+			alert("MetaMask n'est pas installé !");
+			return;
 		}
-];
 
-const iface = new Interface(jsonAbi);
-iface.format(FormatTypes.full);
-console.log(iface);
+		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+		console.log("Connecté avec le compte:", accounts[0]);
+		provider = new ethers.providers.Web3Provider(window.ethereum);
+		const balance = await provider.getBalance(accounts[0]);
+		console.log("Balance:", ethers.utils.formatEther(balance), "ETH");
+		signer = provider.getSigner();
+		contract = new ethers.Contract(contractAddress, ABI, signer);  // Charger le contrat après avoir obtenu le signer
+	} catch (error) {
+		console.error("Erreur lors de la connexion au portefeuille:", error);
+	}
+};
 
-const address = '0x5fbdb2315678afecb367f032d93f642f64180aa3'
 
-const contract = new ethers.Contract(address, ERC20_ABI, provider)
 
-const feur = async () => {
+const reloadContract = async () => {
+	contract = new ethers.Contract(contractAddress, ABI, signer);
+};
+
+//const contract = new ethers.Contract(contractAddress, ABI, signer);
+
+const getNumber = async () => {
+	const number = await contract.number();
+	console.log(`Number: ${number}`);
+};
+
+
+const incr = async () => {
+	const name = await newContract.number()
+	console.log(`Name: ${name}`)
+};
+
+const decr = async () => {
 	const name = await contract.number()
 	console.log(`Name: ${name}`)
 };
 
-// const provider = new ethers.providers.JsonRpcProvider()
-
-// const address = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'
-// const balance = await provider.getBalance(address)
-// console.log(`ETH Balance of ${address} --> ${ethers.utils.formatEther(balance)} ETH\n`)
-// document.getElementById("connectButton").addEventListener("click", connectWallet);
-document.querySelector("#salut").addEventListener("click", feur);
+document.getElementById("connectButton").addEventListener("click", connectWallet);
+document.getElementById("getNumber").addEventListener("click", getNumber);
+document.getElementById("incr").addEventListener("click", incr);
+document.getElementById("decr").addEventListener("click", decr);
