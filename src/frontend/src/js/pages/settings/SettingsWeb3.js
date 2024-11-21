e891cfb60dd5d060b31e150d32bd9deaf705cd7a
Juiceee <lbehr@student.42.fr>
2024-11-21 16:30:02 +0100

    add in bdd pubkey

--- src/frontend/src/js/pages/settings/SettingsWeb3.js
+++ src/frontend/src/js/pages/settings/SettingsWeb3.js
@@ -11,7 +11,12 @@ export class SettingsWeb3 extends Component {
 			<settings-layout>
 				<div class="d-flex flex-column gap-3">
 					<div class="d-flex flex-column gap-1">
-						<h3>Two-Factor Authentication</h3>
+						<h3>Web3 Connection</h3>
+						<button id="connectButton" variant="muted" class="d-flex align-items-center justify-content-center gap-2">
+							Connect Wallet
+						</button>
+						<button id="savePubKey" variant="muted" class="d-flex align-items-center justify-content-center gap-2">Save</button>
+						<button id="oui"> salut</button>
 					</div>
 				</div>
 			</settings-layout>
@@ -19,7 +24,61 @@ export class SettingsWeb3 extends Component {
 	}
 
 	script() {
-		
+		let accounts;
+		document.getElementById("oui").addEventListener("click", () => {
+			if (accounts === undefined) {
+				console.log("undefined");
+			} else {
+				console.log(accounts[0]);
+			}
+		});
+
+		document.getElementById("savePubKey").addEventListener("click", async () => {
+			if (window.auth.profile.publicKey !== "" || accounts === undefined) {
+				document.getElementById("savePubKey").style.disabled = true;
+			} else {
+				document.getElementById("savePubKey").style.disabled = false;
+			}
+			const user = window.auth;
+			console.log("username " + user.username, "public_key " + accounts[0]);
+			try {
+				const { data, error } = await api.request.patch("me/", {
+					"username": user.username,
+					"profile": {
+						"publicKey": accounts[0],
+					}
+				});
+				if (error) throw error;
+				console.log(data);
+				Toast.success("Profile updated");
+			} catch (error) {
+				Toast.error(error.error);
+			}
+		});
+
+		const connectWallet = async () => {
+			try {
+				if (typeof window.ethereum === 'undefined') {
+					alert("MetaMask n'est pas installé !");
+					return;
+				}
+				console.log("Connexion au portefeuille en cours...");
+				document.getElementById("connectButton").innerHTML = "Connexion en cours...";
+				document.getElementById("connectButton").disabled = true;
+				accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+				console.log("Connecté avec le compte:", accounts[0]);
+				document.getElementById("connectButton").innerHTML = "Wallet Connected";
+				Toast.success("Wallet Connected:\n" + accounts[0]);
+
+			}
+			catch (error) {
+				Toast.error("MetaMask: " + error.message);
+				document.getElementById("connectButton").disabled = false;
+				document.getElementById("connectButton").innerHTML = "Connect Wallet";
+			}
+		};
+
+		document.getElementById("connectButton").addEventListener("click", connectWallet);
 	}
 }
 
