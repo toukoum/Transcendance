fe1cbf7a655f16767fb3d9098f06a8e2ac8dd51e
toukoum <raphaelgiraud12@gmail.com>
2024-11-26 21:08:41 +0100

    HTTP done miss image...

--- src/frontend/src/js/pages/settings/SettingsProfile.js
+++ src/frontend/src/js/pages/settings/SettingsProfile.js
@@ -222,8 +222,7 @@ export class SettingsProfile extends Component {
 			formData.append('avatar', file);
 
 			try {
-				// A CHANGER 
-				const response = await fetch('http://localhost:8000/v1/avatar/upload/', {
+				const response = await fetch(`${api.baseUrl}/avatar/upload/`, {
 					method: 'POST',
 					credentials: 'include',
 					body: formData,
