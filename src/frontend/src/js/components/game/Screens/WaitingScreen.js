a240a87599cc31cbd721d08e315b8bf7fe4e8735
loup <loup.quehec@gmail.com>
2024-11-30 12:58:03 +0100

    add scoreboard

--- src/frontend/src/js/components/game/Screens/WaitingScreen.js
+++ src/frontend/src/js/components/game/Screens/WaitingScreen.js
@@ -1,12 +1,20 @@
+import { Toast } from "../../../provider/toast-provider.js";
 import { Component } from "../../../utils/Component.js";
 
 export class WaitingScreen extends Component {
 	content() {
 		const id = this.getAttribute("id");
+		const gameId = this.getAttribute("game-id");
 		return (/*html*/`
 		<div id="${id}" class="waiting-screen position-absolute top-50 start-50 translate-middle p-2 border rounded bg-background w-50">
 			<div class="waiting-screen__content">
-				<h3>Waiting for players<span id="animated-dots"></span></h3>
+				<div class="py-2">
+					<div class="d-flex justify-content-center align-items-center gap-2">
+						<h3 class="text-center">Game</h3>
+						<h3 id="game-id" class="border p-1 rounded text-center" style="cursor: pointer;">${gameId}</h3>
+					</div>
+					<p class="text-center text-muted-foreground">Waiting for players<span id="animated-dots"></span></p>
+				</div>
 				<separator-component></separator-component>
 				<div id="players-list" class="d-flex flex-column gap-2 my-2">
 				</div>
@@ -23,6 +31,14 @@ export class WaitingScreen extends Component {
 			animatedDots.textContent = ".".repeat(dots);
 			dots = (dots + 1) % 4;
 		}, 500);
+
+		// onclick on game-id copy to clipboard
+		const gameIdCopy = this.querySelector("#game-id");
+		gameIdCopy.addEventListener("click", () => {
+			navigator.clipboard.writeText(gameIdCopy.textContent);
+			Toast.success("Game ID copied to clipboard");
+		});
+
 	}
 }
 
